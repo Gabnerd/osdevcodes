8a38b68a8b3c7f2f1ca5507aacacb50a346a1539
Gabnerd <gabriel.pescaroli015@gmail.com>
2022-03-17 00:13:52 -0300

    1703

--- print_hex/print_hex.asm
+++ print_hex/print_hex.asm
@@ -11,10 +11,11 @@ print_hex:
     mov bx, HEX_OUT
     add bx, 2
     shr dx, 8;problema
+    and dx, 0xf
 print_hex_loop:
     cmp byte[bx], 0
     je print_hex_final
-    cmp byte[bx], 58
+    cmp dl, 9
     jg print_hex_letra
 
 print_hex_numero:
@@ -25,7 +26,7 @@ print_hex_numero:
     jmp print_hex_loop
 
 print_hex_letra:
-    mov cx, 96
+    mov cx, 87
     add cx, dx
     mov byte[bx], cl
     add bx, 1
